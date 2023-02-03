1bd352be1979271899834da9f648a778ba7fecbc
keeptrynjoy <sungmin4218@gmail.com>
2023-02-03 16:54:20 +0900

    컴파일 오류를 막기위해 주석처리

--- ch3/src/main/java/com/fastcampus/ch3/diCopy2/Main2.java
+++ ch3/src/main/java/com/fastcampus/ch3/diCopy2/Main2.java
@@ -1,21 +1,21 @@
-package com.fastcampus.ch3.diCopy2;
-
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-
-class Car{}
-class Truck extends Car{}
-class Engine {}
-
-public class Main2 {
-    public static void main(String[] args) throws Exception {
-        AppContext ac = new AppContext();
-        Car car = (Car) ac.getBean("car");
-        Engine engine = (Engine) ac.getBean("engine") ;
-        System.out.println("car = "+ car);
-        System.out.println("Engine = "+ engine);
-    }
-}
+//package com.fastcampus.ch3.diCopy2;
+//
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.util.Properties;
+//
+//class Car{}
+//class Truck extends Car{}
+//class Engine {}
+//
+//public class Main2 {
+//    public static void main(String[] args) throws Exception {
+//        AppContext ac = new AppContext();
+//        Car car = (Car) ac.getBean("car");
+//        Engine engine = (Engine) ac.getBean("engine") ;
+//        System.out.println("car = "+ car);
+//        System.out.println("Engine = "+ engine);
+//    }
+//}
